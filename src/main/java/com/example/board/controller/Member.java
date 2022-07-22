7e8f04a0e1fbaf767847d1a42d3ce2e9c4ff186e
junwoo <top7578@naver.com>
2022-07-23 01:33:21 +0900

    feat: 구글 스타일 적용

--- src/main/java/com/example/board/controller/Member.java
+++ src/main/java/com/example/board/controller/Member.java
@@ -3,11 +3,13 @@ package com.example.board.controller;
 import lombok.Builder;
 import lombok.Getter;
 
-@Builder
-@Getter
+@Builder @Getter
 public class Member {
   Long id;
   String name;
+
+
+
 
   public Member(Long id, String name) {
     this.name = name;
