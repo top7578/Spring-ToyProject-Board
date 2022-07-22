1e0212415656e55ef0ff9770e958aaa0ce5c834d
junwoo <top7578@naver.com>
2022-07-22 22:10:06 +0900

    feat: 데이터 형식에 따른 request 형식 확인

--- src/main/java/com/example/board/controller/Member.java
+++ src/main/java/com/example/board/controller/Member.java
@@ -2,12 +2,15 @@ package com.example.board.controller;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Builder
 @Getter
+@NoArgsConstructor
 public class Member {
+
     Long id;
     String name;
+    String nickname;
     public Member(Long id, String name) {
         this.name = name;
     }
